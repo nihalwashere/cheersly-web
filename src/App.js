1138d6479e6ae22742e6bf1fe3392542259a4ddc
pikure <nihalk2@hotmail.com>
2021-05-12 18:27:41 +0530

    feat: firebase analytics

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from "react";
+import firebase from "firebase/app";
+import "firebase/analytics";
 import RootContainer from "./containers/root";
+import { FIREBASE_CONFIG } from "./utils/config";
 
 const App = () => {
+  if (!firebase.apps.length) {
+    firebase.initializeApp(FIREBASE_CONFIG);
+    firebase.analytics();
+  }
+
   return (
     <div>
       <RootContainer />
