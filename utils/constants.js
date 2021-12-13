2595fe501e7dcf37b9ea6b59215d2f7a6b87694c
pikure <nihalk2@hotmail.com>
2021-12-13 17:46:31 +0530

    fix

--- utils/constants.js
+++
@@ -1,7 +0,0 @@
-export const META_TITLE =
-  "Cheersly · Build an awesome remote work culture in Slack!";
-
-export const META_DESCRIPTION =
-  "Employee engagement and peer recognition platform to build cheerful teams, conduct anonymous polls, share feedback and rewards.";
-
-export const CHEERSLY_SUPPORT_EMAIL = "support@cheersly.club";
